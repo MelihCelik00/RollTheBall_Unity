bf6c19bb57bfe1094e3a6756360b97302201461b
MelihCelik00 <melihsafa.c@gmail.com>
2020-04-13 18:59:57 +0300

    Final Build

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallControl : MonoBehaviour
 {
     Rigidbody physics;
     public int speed;
+    int pointCounter = 0;
+    public int totalLoot;
+    public Text Counter;
+    public Text GameOverText;
 
     void Start()
     {
@@ -21,7 +26,28 @@ public class BallControl : MonoBehaviour
         Vector3 vec = new Vector3(horizontal,0,vertical);
 
         physics.AddForce(vec*speed);
-        //Debug.Log("Horizontal: "+horizontal+" "+ "Vertical: "+vertical);
-        
+        //Debug.Log("Horizontal: "+horizontal+" "+ "Vertical: "+vertical);   
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Touched!");
+        if (other.gameObject.tag == "Loot")
+        {
+            Destroy(other.gameObject); // eats all of the objects on the area
+            pointCounter++;
+
+            Counter.text = "Point Counter: " + pointCounter;
+            //Debug.Log("Point: " + pointCounter);
+
+            if(pointCounter == totalLoot)
+            {
+                //Debug.Log("YOU WIN! Game is over!");
+                GameOverText.text = "Game is over, you win!";
+            }
+        }
+        else if (other.gameObject.tag == "Capsule")
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
--- Assets/Scripts/LootableObjectRotate.cs
+++ Assets/Scripts/LootableObjectRotate.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class LootableObjectRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        transform.Rotate(new Vector3(15, 30, 45)*Time.deltaTime);
+        //Debug.Log(Time.deltaTime);
     }
 }
