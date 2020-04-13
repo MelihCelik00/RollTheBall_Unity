620d8d5cf58f2501a4739b89cded1b449270eefc
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2020-04-13 17:46:39 +0300

    First commit

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    Rigidbody physics;
+    public int speed;
+
+    void Start()
+    {
+        physics = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(horizontal,0,vertical);
+
+        physics.AddForce(vec*speed);
+        //Debug.Log("Horizontal: "+horizontal+" "+ "Vertical: "+vertical);
+        
+    }
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject ball;
+    Vector3 distanceBetween;
+    void Start()
+    {
+        distanceBetween = transform.position - ball.transform.position;
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = ball.transform.position + distanceBetween;
+    }
+}
